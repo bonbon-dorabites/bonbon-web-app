2730551e8ac87ef058dc6b5330be496b19c0d1ed
Shayne Bondoc <shaynefbondoc@gmail.com>
2025-02-01 19:25:33 +0800

    added the imports

---
+++ menus/dashboards/js/order.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { getFirestore, collection, addDoc, doc, getDoc, where, getDocs, query, onSnapshot } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB2ACxlgsaO0_E2zA1zsPEntCXOIHaG21I",
+    authDomain: "bonbon-8a34a.firebaseapp.com",
+    projectId: "bonbon-8a34a",
+    storageBucket: "bonbon-8a34a.firebasestorage.app",
+    messagingSenderId: "276254510771",
+    appId: "1:276254510771:web:b936bce5f45ed255b56ac6",
+    measurementId: "G-85BQTNL30R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+auth.languageCode = 'en';
+
