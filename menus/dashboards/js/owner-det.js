f3d856a467bbc4a16973f6f8dd891bb643819599
Jetty <jethrojamesaguilar.18@gmail.com>
2025-02-26 14:16:48 +0800

    test2

--- menus/dashboards/js/owner-det.js
+++ menus/dashboards/js/owner-det.js
@@ -257,7 +257,7 @@ submitBtn.addEventListener("click", async () => {
                         await updateDoc(doc.ref, { passcode: newPassword }); // Store hashed password if needed
                         showModal("You have changed your password. You need to log-in again.");
                         setTimeout(() => {
-                            window.location.href = "../../../../bonbon-web-app/auth/login.html";
+                            window.location.href = "../../../auth/login.html";
                         }, 2000);
                     });
                 }
